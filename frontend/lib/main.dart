e12126572c56dd1bce1ef8dfb58aed9990ac774a
shirayukisoku <iskander.ruzeyev@gmail.com>
2025-07-06 02:39:41 +0500

    FrontendBranchv1

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,32 @@
 //Точка входа приложения
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Flutter!'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
