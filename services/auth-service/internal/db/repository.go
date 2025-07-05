8a62b1d77eff0d7285a88d03223c385f69bc506c
BSanjik <kundyz.sanjik@gmail.com>
2025-07-05 13:13:41 +0500

    RegistraitionServiceV1+test

--- services/auth-service/internal/db/repository.go
+++ services/auth-service/internal/db/repository.go
@@ -3,13 +3,34 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
+	"time"
 
 	"github.com/BSanjik/LUNARY/services/auth-service/internal/config"
+
+	_ "github.com/lib/pq"
 )
 
 func InitDB(cfg *config.Config) (*sql.DB, error) {
-	connsStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
-	return sql.Open("postgres", connsStr)
+	log.Println("[DB] Connecting to:", cfg.DBUrl) //после проверки удалить
+
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		log.Println("[DB] Failed to open DB:", err)
+		return nil, err
+	}
+
+	//connection check
+	for i := 1; i <= 10; i++ {
+		err := db.Ping()
+		if err == nil {
+			log.Println("[DB] Successfully connected")
+			return db, nil
+		}
+		log.Printf("[DB] Ping attempt %d failed: %v", i, err)
+		time.Sleep(2 * time.Second)
+	}
+
+	log.Println("[DB] Successfully connected")
+	return db, nil
 }
--- services/auth-service/internal/handler/handler.go
+++ services/auth-service/internal/handler/handler.go
@@ -4,9 +4,11 @@ package handler
 import (
 	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
-	"github.com/BSanjik/LUNARY/services/auth-service/internal/model"
 	"github.com/BSanjik/LUNARY/services/auth-service/internal/token"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -15,15 +17,77 @@ type AuthHandler struct {
 	DB *sql.DB
 }
 
+type registerRequest struct {
+	Phone    string `json:"phone"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type registerResponse struct {
+	Token string `json:"token"`
+}
+
+var phoneRegex = regexp.MustCompile(`^\+?[0-9]{10,15}$`)
+
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
-	var u model.User
-	json.NewDecoder(r.Body).Decode(&u)
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-	err := h.DB.QueryRow("INSERT INTO users(phone, password, email) VALUES($1, $2) RETURNING id", u.Phone, string(hashed), u.Email).Scan(&u.ID)
-	if err != nil {
-		http.Error(w, "Email already exists", http.StatusBadRequest)
+	var req registerRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		log.Println("[Register] JSON decode error:", err)
 		return
 	}
-	tokenStr, _ := token.GenerateToken(u.ID)
-	w.Write([]byte(tokenStr))
+
+	req.Phone = strings.TrimSpace(req.Phone)
+	req.Password = strings.TrimSpace(req.Password)
+	req.Email = strings.TrimSpace(req.Email)
+
+	//phone and pass validation
+	if !phoneRegex.MatchString(req.Phone) {
+		http.Error(w, "Неверный формат телефона", http.StatusBadRequest)
+		return
+	}
+	if len(req.Password) < 12 {
+		http.Error(w, "Пароль должен содержать 12 символов", http.StatusBadRequest)
+		return
+	}
+
+	//isExist validation
+	var isExist bool
+	err := h.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE phone=$1)", req.Phone).Scan(&isExist)
+	if err != nil {
+		http.Error(w, "DB Error ", http.StatusInternalServerError)
+		log.Println("[Register] DB error:", err)
+		return
+	}
+	if isExist {
+		http.Error(w, "Пользователь с таким номером уже существует", http.StatusConflict)
+		return
+	}
+
+	//hash pass
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+
+	//insert DB
+	var userID int64
+	err = h.DB.QueryRow("INSERT INTO users (phone, password, email) VALUES ($1, $2, $3) RETURNING id", req.Phone, string(hashedPass), req.Email).Scan(&userID)
+	if err != nil {
+		http.Error(w, "Ошибка регистрации клиента", http.StatusInternalServerError)
+		return
+	}
+
+	//JWT generation
+	tokenStr, err := token.GenerateToken(userID)
+	if err != nil {
+		http.Error(w, "Token generation failed", http.StatusInternalServerError)
+		return
+	}
+
+	//Response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(registerResponse{Token: tokenStr})
+
 }
